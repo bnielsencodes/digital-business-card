ce5ba504dd0c40438996ad9bbf4054259d627f13
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-10-23 19:08:52 -0700

    Create Info.js

---
+++ src/components/Info.js
@@ -0,0 +1,23 @@
+import React from "react";
+import profilePic from "../assets/images/ben-sweet-unsplash.jpg";
+import "../index.css";
+
+export default function Info() {
+  return (
+    <div className="info">
+      <img
+        className="profile-pic"
+        src={profilePic}
+        alt="man in black beanie cap"
+      />
+      <div className="info-bottom">
+        <h1>Brandon Nielsen</h1>
+        <h4>Front-End Web Developer</h4>
+        <a href="https://bnielsen.dev" target="_blank">
+          <small>bnielsen.dev</small>
+        </a>
+        <Button />
+      </div>
+    </div>
+  );
+}
