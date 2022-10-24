9417285f74b3bac39470033bc3cd76d9b53b5f7c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-10-23 19:09:24 -0700

    Create Interests.js

---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css";
+
+export default function Interests() {
+  return (
+    <div className="interests">
+      <h2>Interests</h2>
+      <p>Coding, gaming, cooking, hiking and spending time with my family.</p>
+    </div>
+  );
+}
