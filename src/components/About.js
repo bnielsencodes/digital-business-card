b6b6c26bc83c4a3d381e426de090782d39f55864
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-10-23 19:09:19 -0700

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../index.css";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <p>
+        Fully committed to the philosophy of life-long learning, I’m a front-end
+        web developer with a deep passion for all things web development.
+      </p>
+      <p>
+        Coming from a background in behavioral health, I bring an analytical yet
+        creative approach to problem solving that lends itself well to the
+        design and creation of aesthetic and intuitive user experiences.
+      </p>
+    </div>
+  );
+}
